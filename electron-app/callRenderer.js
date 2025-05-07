a33fce22980f5e343e613e8809da2bb654e2f4e2
Piacquadio Mirko <piacquadio.mirko@gmail.com>
2025-05-07 21:30:06 +0200

    Fine

--- electron-app/callRenderer.js
+++ electron-app/callRenderer.js
@@ -67,13 +67,16 @@ async function startCall(isCaller) {
     //   }
     // ]
     iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' },
-      {
-        urls: 'turn:openrelay.metered.ca:80',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-      }
+      { urls: 'stun:stun.l.google.com:19302' }
     ]
+    // iceServers: [
+    //   { urls: 'stun:stun.l.google.com:19302' },
+    //   {
+    //     urls: 'turn:openrelay.metered.ca:80',
+    //     username: 'openrelayproject',
+    //     credential: 'openrelayproject'
+    //   }
+    // ]
   });
 
   pc.onicecandidate = event => {
