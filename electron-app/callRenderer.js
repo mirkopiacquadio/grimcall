e3ae7265fbc1ec3bb5632dce3259f353c9615077
Piacquadio Mirko <piacquadio.mirko@gmail.com>
2025-05-07 22:00:41 +0200

    Fine

--- electron-app/callRenderer.js
+++ electron-app/callRenderer.js
@@ -80,7 +80,12 @@ async function startCall(isCaller) {
 
   pc = new RTCPeerConnection({
     iceServers: [
-      { urls: 'stun:stun.l.google.com:19302' }
+      { urls: 'stun:stun.l.google.com:19302' },
+      {
+        urls: 'turn:openrelay.metered.ca:80',
+        username: 'openrelayproject',
+        credential: 'openrelayproject'
+      }
     ]
   });
 
