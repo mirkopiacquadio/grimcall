5476ef787f81fdebd833b23375a983218969a9f9
Piacquadio Mirko <piacquadio.mirko@gmail.com>
2025-06-22 15:51:36 +0200

    Fine

--- electron-app/callRenderer.js
+++ electron-app/callRenderer.js
@@ -17,6 +17,7 @@ const localVideo = document.getElementById('localVideo');
 const remoteVideos = document.getElementById('remoteVideos') || makeRemoteVideosContainer();
 const callStatus = document.getElementById('callStatus');
 const endCallBtn = document.getElementById('endCallBtn');
+const incomingCallAudio = document.getElementById('incomingCallAudio');
 
 function log(...args) {
   console.log('[CALL]', ...args);
@@ -170,8 +171,13 @@ function setupWebSocket() {
       }
     }
     if (data.type === 'incoming-call' && data.invite) {
-      log('Sei stato invitato a una chiamata!', data);
-      // TODO: UI per accetta/rifiuta
+      if (data.type === 'incoming-call' && data.invite) {
+        log('Sei stato invitato a una chiamata!', data);
+        if (incomingCallAudio) {
+          incomingCallAudio.currentTime = 0; // Riavvia da inizio
+          incomingCallAudio.play().catch(e => log('Audio play error', e));
+        }
+      }
     }
     if (data.type === 'participant-left') {
       log('Peer ha lasciato:', data.name);
